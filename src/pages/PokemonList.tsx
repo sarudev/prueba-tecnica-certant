43985a6bb2007fd4b9a03fc3b4d5c003d4b4f349
sarudev <josecoria959@gmail.com>
2023-07-12 21:50:35 -0300

    changed scrollbar style due to bug

--- src/pages/PokemonList.tsx
+++ src/pages/PokemonList.tsx
@@ -3,6 +3,7 @@ import PokemonItem from '../components/PokemonItem'
 import useInfiniteScroll from '../hooks/useInfiniteScroll'
 import { PokemonItemWithoutTypeText } from '../components/PokemonItemWithoutTypeText'
 import '../styles/pokemonList.scss'
+import { Link } from 'react-router-dom'
 
 export default function PokemonList () {
   const { pokemones, fetchMore } = usePokemones(10)
@@ -10,7 +11,7 @@ export default function PokemonList () {
 
   return (
     <ul className='pokemon-list' ref={pokeListRef} onScroll={handleScroll}>
-      {pokemones.map((pokemon) => <a href={`/pokedex/${pokemon.id}`} key={pokemon.id}><PokemonItem pokemon={pokemon} /></a>)}
+      {pokemones.map((pokemon) => <Link to={`/pokedex/${pokemon.id}`} key={pokemon.id}><PokemonItem pokemon={pokemon} /></Link>)}
       <PokemonItemWithoutTypeText types={['unknown']} name='Loading...' baseExperience='N/A' />
     </ul>
   )
